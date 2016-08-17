abdc86907fe10bf6303544d2020d7b699bc36cbb
corn <roger199585@yahoo.com.tw>
2016-08-18 00:17:09 +0800

    update 8/17

--- src/TodoList/components/Todo/TodoList.jsx
+++ src/TodoList/components/Todo/TodoList.jsx
@@ -18,6 +18,7 @@ export default class TodoList extends Component {
 
 TodoList.propTypes = {
   onTodoClick: PropTypes.func.isRequired,
+  onDelClick: PropTypes.func.isRequired,
   todos: PropTypes.arrayOf(PropTypes.shape({
   text: PropTypes.string.isRequired,
   completed: PropTypes.bool.isRequired
